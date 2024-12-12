683bd53eb1cbf64e7e8773c2519689ceb2be8c8d
James Koster <james@jameskoster.co.uk>
2024-12-12 10:21:51 +0000

    Make it work for now

---
+++ storybook/stories/foundations/layout/headers-code.tsx
@@ -0,0 +1,14 @@
+/**
+ * External dependencies
+ */
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import React from 'react';
+
+/**
+ * WordPress dependencies
+ */
+import { Button } from '@wordpress/components';
+
+export const HeaderExample = () => {
+	return <Button variant="primary">Click me</Button>;
+};
