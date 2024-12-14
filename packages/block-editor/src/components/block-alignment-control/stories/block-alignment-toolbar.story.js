1bb38e671c48c4fcc24e4fa448bb1aff002d0d3e
akasunil <sdprajapati999@gmail.com>
2024-12-14 11:45:11 +0530

    Update doc and story for other props of component

--- packages/block-editor/src/components/block-alignment-control/stories/block-alignment-toolbar.story.js
+++ packages/block-editor/src/components/block-alignment-control/stories/block-alignment-toolbar.story.js
@@ -14,10 +14,12 @@ import { BlockAlignmentToolbar } from '../';
 const meta = {
 	title: 'BlockEditor/BlockAlignmentToolbar',
 	component: BlockAlignmentToolbar,
+	parameters: {
+		docs: { canvas: { sourceState: 'shown' } },
+	},
 	argTypes: {
 		value: {
 			control: { type: null },
-			defaultValue: 'undefined',
 			description: 'The current value of the alignment setting.',
 		},
 		onChange: {
@@ -25,6 +27,14 @@ const meta = {
 			control: { type: null },
 			description:
 				"A callback function invoked when the toolbar's alignment value is changed via an interaction with any of the toolbar's buttons. Called with the new alignment value (ie:`left`, `center`, `right`, `wide`, and `full`) as the only argument.",
+		},
+		controls: {
+			control: { type: null },
+			description: 'An array of available alignment controls.',
+		},
+		isCollapsed: {
+			control: { type: null },
+			description: 'Whether the toolbar should be collapsed.',
 		},
 	},
 };
