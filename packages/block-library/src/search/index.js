2db363aef52f776f1a405aab4d33013dc2747df9
Michal Czaplinski <mmczaplinski@gmail.com>
2025-01-06 15:18:28 +0000

    Update Search block label in ListView when it's an Instant Search
    - Updated `getBlockLabel` to include `clientId` for improved label context.
    - Modified `useBlockDisplayTitle` to utilize the updated `getBlockLabel` function.
    - Introduced `__experimentalLabel` in the search settings to handle block labels based on query loop context and instant search status.

--- packages/block-library/src/search/index.js
+++ packages/block-library/src/search/index.js
@@ -1,8 +1,10 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { search as icon } from '@wordpress/icons';
+import { select } from '@wordpress/data';
+import { store as blockEditorStore } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -18,6 +20,41 @@ export { metadata, name };
 
 export const settings = {
 	icon,
+	__experimentalLabel( attributes, { clientId } ) {
+		const { label } = attributes;
+		const customName = attributes?.metadata?.name;
+
+		// Check if the block is inside a Query Loop block.
+		const queryLoopBlockIds = select(
+			blockEditorStore
+		).getBlockParentsByBlockName( clientId, 'core/query' );
+
+		// If the block is not inside a Query Loop block, return the block label.
+		if ( ! queryLoopBlockIds.length ) {
+			return customName || label;
+		}
+
+		const queryLoopBlock = select( blockEditorStore ).getBlock(
+			queryLoopBlockIds[ 0 ]
+		);
+
+		// Check if the Query Loop block has enhanced pagination enabled and
+		// if the `__experimentalEnableSearchQueryBlock` flag is enabled.
+		const hasInstantSearch = !! (
+			queryLoopBlock?.attributes?.enhancedPagination &&
+			window?.__experimentalEnableSearchQueryBlock
+		);
+
+		if ( ! hasInstantSearch ) {
+			return customName || label;
+		}
+
+		return sprintf(
+			/* translators: %s: The block label */
+			__( '%s (Instant search enabled)' ),
+			customName || label
+		);
+	},
 	example: {
 		attributes: { buttonText: __( 'Search' ), label: __( 'Search' ) },
 		viewportWidth: 400,
