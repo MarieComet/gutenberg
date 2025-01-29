1f258560703b655c324a33b7efa44f0aa7ae319d
Hiroshi Urabe <mail@torounit.com>
2025-01-29 17:44:35 +0900

    convert font weight number to string

--- packages/edit-site/src/components/global-styles/font-library-modal/utils/constants.js
+++ packages/edit-site/src/components/global-styles/font-library-modal/utils/constants.js
@@ -7,47 +7,47 @@ export const ALLOWED_FILE_EXTENSIONS = [ 'otf', 'ttf', 'woff', 'woff2' ];
 
 export const FONT_WEIGHTS = [
 	{
-		value: 100,
+		value: '100',
 		label: _x( 'Thin', 'font weight' ),
 		slug: 'thin',
 	},
 	{
-		value: 200,
+		value: '200',
 		label: _x( 'Extra-light', 'font weight' ),
 		slug: 'extra-light',
 	},
 	{
-		value: 300,
+		value: '300',
 		label: _x( 'Light', 'font weight' ),
 		slug: 'light',
 	},
 	{
-		value: 400,
+		value: '400',
 		label: _x( 'Normal', 'font weight' ),
 		slug: 'normal',
 	},
 	{
-		value: 500,
+		value: '500',
 		label: _x( 'Medium', 'font weight' ),
 		slug: 'medium',
 	},
 	{
-		value: 600,
+		value: '600',
 		label: _x( 'Semi-bold', 'font weight' ),
 		slug: 'semi-bold',
 	},
 	{
-		value: 700,
+		value: '700',
 		label: _x( 'Bold', 'font weight' ),
 		slug: 'bold',
 	},
 	{
-		value: 800,
+		value: '800',
 		label: _x( 'Extra-bold', 'font weight' ),
 		slug: 'extra-bold',
 	},
 	{
-		value: 900,
+		value: '900',
 		label: _x( 'Black', 'font weight' ),
 		slug: 'black',
 	},
